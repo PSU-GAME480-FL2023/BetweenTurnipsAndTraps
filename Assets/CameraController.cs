d4b2d895658f4bc68657f62c0b7fb926b4bcdd14
Kenneth Mitchell Burt <kmb898@psu.edu>
2023-11-15 13:41:59 -0500

    Scene Change and Hub Setup

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,12 +7,6 @@ public class CameraController : MonoBehaviour
     public bool InDungeon;
     public Transform player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
