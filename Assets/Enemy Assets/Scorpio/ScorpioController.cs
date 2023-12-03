e7c38cd32fbb9e3bab83370bbaff6a0c0981c021
Kenneth Mitchell Burt <kmb898@psu.edu>
2023-12-03 17:46:37 -0500

    Removed Scorpio Tick

--- Assets/Enemy Assets/Scorpio/ScorpioController.cs
+++ Assets/Enemy Assets/Scorpio/ScorpioController.cs
@@ -35,43 +35,33 @@ public class ScorpioController : MonoBehaviour
 
     void Shoot()
     {
-        if(tick == firerate)
+        //Fire in specified direction
+        if (fireDirection.ToString() == "Up")
         {
-            //Reset tick
-            tick = 0;
-
-            //Fire in specified direction
-            if (fireDirection.ToString() == "Up")
-            {
-                var newProj = Instantiate(projectile, new Vector3(t.localPosition.x, t.localPosition.y + 0.375f, -2), Quaternion.Euler(0, 0, 0));
-                var temp = newProj.GetComponent<Rigidbody2D>();
-                temp.velocity = new Vector2(0, firespeed);
-            }
-
-            else if (fireDirection.ToString() == "Left")
-            {
-                var newProj = Instantiate(projectile, new Vector3(t.localPosition.x - 0.375f, t.localPosition.y + 0.5F, -2), Quaternion.Euler(0, 0, 90));
-                var temp = newProj.GetComponent<Rigidbody2D>();
-                temp.velocity = new Vector2(-firespeed, 0);
-            }
-
-            else if (fireDirection.ToString() == "Right")
-            {
-                var newProj = Instantiate(projectile, new Vector3(t.localPosition.x + 0.375f, t.localPosition.y + 0.5f, -2), Quaternion.Euler(0, 0, 270));
-                var temp = newProj.GetComponent<Rigidbody2D>();
-                temp.velocity = new Vector2(firespeed, 0);
-            }
-
-            else
-            {
-                var newProj = Instantiate(projectile, new Vector3(t.localPosition.x, t.localPosition.y - 0.375f, -2), Quaternion.Euler(0, 0, 180));
-                var temp = newProj.GetComponent<Rigidbody2D>();
-                temp.velocity = new Vector2(0, -firespeed);
-            }
+            var newProj = Instantiate(projectile, new Vector3(t.localPosition.x, t.localPosition.y + 0.375f, -2), Quaternion.Euler(0, 0, 0));
+            var temp = newProj.GetComponent<Rigidbody2D>();
+            temp.velocity = new Vector2(0, firespeed);
         }
+
+        else if (fireDirection.ToString() == "Left")
+        {
+            var newProj = Instantiate(projectile, new Vector3(t.localPosition.x - 0.375f, t.localPosition.y + 0.5F, -2), Quaternion.Euler(0, 0, 90));
+            var temp = newProj.GetComponent<Rigidbody2D>();
+            temp.velocity = new Vector2(-firespeed, 0);
+        }
+
+        else if (fireDirection.ToString() == "Right")
+        {
+            var newProj = Instantiate(projectile, new Vector3(t.localPosition.x + 0.375f, t.localPosition.y + 0.5f, -2), Quaternion.Euler(0, 0, 270));
+            var temp = newProj.GetComponent<Rigidbody2D>();
+            temp.velocity = new Vector2(firespeed, 0);
+        }
+
         else
         {
-            tick++;
+            var newProj = Instantiate(projectile, new Vector3(t.localPosition.x, t.localPosition.y - 0.375f, -2), Quaternion.Euler(0, 0, 180));
+            var temp = newProj.GetComponent<Rigidbody2D>();
+            temp.velocity = new Vector2(0, -firespeed);
         }
     }
 }
